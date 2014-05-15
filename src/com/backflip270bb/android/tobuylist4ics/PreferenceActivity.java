928a3733c239b55dbc07ac1ed6cf639a135f4e20
u-one <backflip270bb@gmail.com>
2014-05-15 21:36:40 +0900

    [mod] create strings for preferences

---
+++ src/com/backflip270bb/android/tobuylist4ics/PreferenceActivity.java
@@ -0,0 +1,29 @@
+package com.backflip270bb.android.tobuylist4ics;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class PreferenceActivity extends Activity {
+	public static final String KEY_ITEMLIST_ADD_IMMEDIATELY = "itemlist_add_immediately";
+	public static final String KEY_NOTIFICATION_ENABLED = "notification_enabled";
+	public static final String KEY_NOTIFICATION_VIBE = "notification_vibe";
+	public static final String KEY_NOTIFICATION_SOUND = "notification_sound";
+	public static final String KEY_NOTIFICATION_RINGTONE = "notification_ringtone";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getFragmentManager().beginTransaction()
+				.replace(android.R.id.content, new AppPreferenceFragment())
+				.commit();
+	}
+
+	public static class AppPreferenceFragment extends PreferenceFragment {
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.xml.preferences);
+		}
+	}
+}
